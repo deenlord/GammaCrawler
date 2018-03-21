443f2b15db9472528ea988a62e3dd64e3b1a5cd7
deenlord <36308232+deenlord@users.noreply.github.com>
2018-03-21 09:10:40 -0600

    Create User.java
    just staging. will clean up better later.... sorry.

---
+++ User.java
@@ -0,0 +1,16 @@
+package com.jeromierand.gammapos;
+
+class User extends Character {
+
+	public User(String name) {
+		this.name = name;
+		hp = 100;
+		inv = new Inventory();
+		main = new Weapon("Wooden Sword", 1);
+		shield = new Shield("Wooden Shield", 1);
+
+		inv.add(main);
+		inv.add(shield);
+	}
+
+}
