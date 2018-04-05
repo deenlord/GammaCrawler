0f0aba3c7f3745d06aa9d7b37b5eca14c14756ed
Christian Rathke <36272735+Crathke4@users.noreply.github.com>
2018-04-05 09:48:36 -0600

    Input Mover 2 is fully working

--- src/com/gammacrawler/test/inputMover2.java
+++ src/com/gammacrawler/test/inputMover2.java
@@ -1,46 +1,87 @@
 package com.gammacrawler.test;
 
-import java.util.Scanner;
-
-import com.gammacrawler.Direction;
 import com.gammacrawler.User;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;;
 /**
  * 
  * @author crathke4
  *
  */
-public class inputMover2 
+public class inputMover2 extends Application
 {
 	User user=new User("Richard");//(Goes by Dick)
+	public static boolean keepGoing=true;
+	Stage MainStage;
 	
 	/**
 	 * 
 	 * @return Direction based on WSAD input from console.
 	 */
-	public Direction DirectionIn()
+
+	public Text label=new Text();
+	private void moveIn(Scene scene)
 	{
-		Scanner scn=new Scanner(System.in);
-		System.out.println("Standard WSAD controls: ");
-		String input=scn.next().toLowerCase();
-		Direction move=null;
-		switch(input)
-		{
-		case "w": 
-			move=Direction.NORTH;
-			break;
-		case "s": 
-			move=Direction.SOUTH;
-			break;
-		case "a": 
-			move=Direction.WEST;
-			break;
-		case "d": 
-			move=Direction.EAST;
-			break;
-		default:
-			move=DirectionIn();
-			break;
-		}
-		return move;
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override public void handle(KeyEvent e) {
+				switch(e.getCode()){
+				case W:
+					//code for NORTH
+					label.setText("W");
+					System.out.println("It works...");
+					break;
+				case S:
+					//code for SOUTH
+					label.setText("S");
+					System.out.println("It works...");
+					break;
+				case A:
+					//code for WEST
+					label.setText("A");
+					System.out.println("It works...");
+					break;
+				case D:
+					//code for EAST
+					label.setText("D");
+					System.out.println("It works...");
+					break;
+				default:
+					System.out.println("It doesn't works...");
+					break;
+				}
+				
+			}
+		});
+	}
+	
+	public void start(Stage stage)
+	{
+		Pane pane=new Pane();
+		label.setText("Default");
+		Text testLabel=new Text("Defacto");
+		HBox hb=new HBox();
+		pane.getChildren().add(label);
+		pane.getChildren().add(testLabel);
+		hb.getChildren().add(testLabel);
+		hb.getChildren().add(label);
+	//	this.MainStage=stage;
+		Scene scene=new Scene(hb, 500, 500);
+		moveIn(scene);
+		stage.setScene(scene);
+		stage.setTitle("InputMover 2.0");
+		stage.show();
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		launch(args);
 	}
 }
