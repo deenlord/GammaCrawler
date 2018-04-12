5826d0df1c37ed183522dcb80ecdb904fb37da18
Christian Rathke <36272735+Crathke4@users.noreply.github.com>
2018-04-12 13:16:24 -0600

    Create StatusBar
    Currently only has a health bar

---
+++ src/com/gammacrawler/StatusBar.java
@@ -0,0 +1,47 @@
+package com.gammacrawler;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+
+public class StatusBar extends Pane{
+	
+	private User player;
+	private Rectangle border;
+	private Rectangle health;
+	private Shape healthBar= Shape.union(border, health);
+	private Rectangle experienceBar;
+	
+	/**
+	 * 
+	 * @param gen-Generator used to read player Health
+	 * @param height-Height of Status Bar
+	 * @param length-Length of each bar
+	 */
+	StatusBar(Generator gen, int height, int length)
+	{
+		this.player=gen.getPlayer();
+		this.getChildren().add(generateHealthBar(60,800));
+	}
+	
+	private Shape generateHealthBar(int height, int length)
+	{
+		border=new Rectangle(0,0,height,length);
+		Rectangle darkHealth=new Rectangle(5,5,(player.getHP()/player.getMaxHP())*(length-10),(height/2)-10),
+				 lightHealth=new Rectangle(5,darkHealth.getHeight()+5,(player.getHP()/player.getMaxHP())*(length-10),(height/2)-10);
+		health=(Rectangle) Shape.union(lightHealth, darkHealth);
+		border.setFill(Color.BLACK);
+		health.setFill(Color.DARKRED);
+		update();
+		return healthBar;
+	}
+
+	
+	public void update()
+	{
+		health.setWidth(((double)player.getHP()/(double)player.getMaxHP())*(800-10));
+	}
+
+
+}
