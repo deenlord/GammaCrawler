9dc957c8e01fd2b44175acce74be822b47e6cdb7
deenlord <pointypawsmcpawdleton@gmail.com>
2018-04-21 07:29:45 -0600

    added GoldPotion

---
+++ src/com/gammacrawler/item/GoldPotion.java
@@ -0,0 +1,48 @@
+package com.gammacrawler.item;
+
+import com.gammacrawler.Direction;
+import com.gammacrawler.Generator;
+import com.gammacrawler.entity.Character;
+import com.gammacrawler.entity.Entity;
+import com.gammacrawler.entity.Item;
+import com.gammacrawler.entity.Sprite;
+
+public class GoldPotion extends Potion {
+	private static final String name = "Health Potion";
+	private static final int value = 25;
+	private static final Sprite sprite = new Sprite("file:src/com/cammacralwer/images/smallhealthpotion.png");
+	
+	public GoldPotion() {
+		super(name, sprite, value);
+	}
+
+	@Override
+	public void drink(Character c) {
+		// TODO Auto-generated method stub
+		int index  = 0;
+		
+		Generator.player.setPoints(Generator.player.getPoints() + value);
+		
+		for (int i = 0; i < c.getInventory().size(); i++) {
+			Item in = c.getInventory().get(i);
+			if(in instanceof GoldPotion) {
+				index  = i;
+				c.getInventory().remove(index);
+			}
+		}
+	
+	}
+
+	@Override
+	public void collide(Entity e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void move(Direction d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
