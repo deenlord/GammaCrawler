5bfbc68b7c2a6acd07bc678fcbb2fefd93f007d2
WolfieWaffle <wolfie.waffle11@gmail.com>
2018-04-23 23:54:59 -0600

    Add some comments and a new constructor

--- src/com/gammacrawler/generator/populators/PopulatorEnemies.java
+++ src/com/gammacrawler/generator/populators/PopulatorEnemies.java
@@ -8,10 +8,16 @@ import com.gammacrawler.entity.Entity;
 import com.gammacrawler.item.RandomItem;
 
 public class PopulatorEnemies extends Populator {
-	private static int MaxItemsInEnemyInventory = 2;
+	private static int maxItemsInEnemyInventory;
+
+	public PopulatorEnemies(int[][] tileArray, ArrayList<Entity> entities, int maxInventoryItems) {
+		this(tileArray, entities);
+		maxItemsInEnemyInventory = maxInventoryItems;
+	}
 
 	public PopulatorEnemies(int[][] tileArray, ArrayList<Entity> entities) {
 		super(tileArray, entities);
+		maxItemsInEnemyInventory = 2;
 	}
 
 	@Override
@@ -24,22 +30,27 @@ public class PopulatorEnemies extends Populator {
 			ogre = new Ogre();
 
 			// Add random items
-			int itemCount = (int) ((Math.random() * MaxItemsInEnemyInventory) + 1);
+			int itemCount = (int) ((Math.random() * maxItemsInEnemyInventory) + 1);
 			for (int j = 0; j < itemCount; j++) {
 				ogre.addToInventory(new RandomItem().getItem());
 			}
 
+			// Move Ogre to random location
 			ogre.moveToTile(empt[1] + 1, empt[0] + 1); // TODO: Fix entity coordinates
+
 			empt = getRandomFreeSpace();
 			slime = new Slime();
 
 			// Add random items
-			itemCount = (int) ((Math.random() * MaxItemsInEnemyInventory) + 1);
+			itemCount = (int) ((Math.random() * maxItemsInEnemyInventory) + 1);
 			for (int j = 0; j < itemCount; j++) {
 				slime.addToInventory(new RandomItem().getItem());
 			}
 
+			// Move Slime to random location
 			slime.moveToTile(empt[1] + 1, empt[0] + 1);
+
+			// Add the entities to the list
 			entities.add(ogre);
 			entities.add(slime);
 			
