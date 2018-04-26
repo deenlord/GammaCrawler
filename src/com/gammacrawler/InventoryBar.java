a9188470470331148ef0f9dd42978812eaef9d8f
deenlord <pointypawsmcpawdleton@gmail.com>
2018-04-26 11:57:34 -0600

    inventory bar positioning.

--- src/com/gammacrawler/InventoryBar.java
+++ src/com/gammacrawler/InventoryBar.java
@@ -3,6 +3,7 @@ package com.gammacrawler;
 import java.util.ArrayList;
 
 import com.gammacrawler.entity.Item;
+import com.gammacrawler.item.Weapon;
 
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -15,6 +16,7 @@ public class InventoryBar extends Pane {
 
 	public InventoryBar() {
 		this.setupInventoryBar();
+		this.setPrefSize(Settings.TILESIZE * 9, Settings.TILESIZE);
 	}
 
 	public void setupInventoryBar() {
@@ -22,13 +24,20 @@ public class InventoryBar extends Pane {
 		ArrayList<Item> inv = Generator.player.getInventory();
 
 		for (int i = 0; i < inv.size(); i++) {
-			ImageView iv = inv.get(i).getSprite().getNewImageView();
-			iv.setX(i * Settings.TILESIZE);
-			iv.setY(Settings.TILESIZE);
-
-			this.getChildren().add(iv);
+			
+			
+			
+				ImageView iv = inv.get(i).getSprite().getNewImageView();
+				iv.setX(i * Settings.TILESIZE);
+				iv.setY(Settings.TILESIZE);
+	
+				this.getChildren().add(iv);
+				
+				if (inv.get(i) instanceof Weapon) {
+					iv.setVisible(false);
+				}
+			}
 		}
-	}
 
 	public void update() {
 		this.setupInventoryBar();
