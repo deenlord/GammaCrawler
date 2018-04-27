052e3269a240358fe07af8c1231ce74da14749d0
deenlord <pointypawsmcpawdleton@gmail.com>
2018-04-26 20:58:40 -0600

    added Witch and ZombieNinja collisions to Weapon class in this commit. Somewhere in that last merge I fixed the offset of the InventoryBar, it is now aligned correctly. Not sure what happend. I was 3 commits ahead of master, pulled, merged, fixed conflicts, pushed. I have the changes locally and all looks good.  I think the last commit closes #47 and closes #48 Will re-open if it's broken.

--- src/com/gammacrawler/item/Weapon.java
+++ src/com/gammacrawler/item/Weapon.java
@@ -4,6 +4,8 @@ import com.gammacrawler.Generator;
 import com.gammacrawler.StatusBar;
 import com.gammacrawler.enemies.Ogre;
 import com.gammacrawler.enemies.Slime;
+import com.gammacrawler.enemies.Witch;
+import com.gammacrawler.enemies.ZombieNinja;
 import com.gammacrawler.entity.Entity;
 import com.gammacrawler.entity.Item;
 import com.gammacrawler.entity.Sprite;
@@ -54,6 +56,10 @@ public abstract class Weapon<T> extends Item {
 	
 	@Override
 	public void collide(Entity e) {
+		
+		// unfortunately, we have not figured out a way to damage
+		// just any enemy. I would have liked this to be more elegant
+		// #Deenlord 4/26/18
 		if (isDoingDamage) {
 			int rand = (int) Math.random() * 100;
 			if (e instanceof Slime) {
@@ -68,10 +74,28 @@ public abstract class Weapon<T> extends Item {
 			else if (e instanceof Ogre) {
 				if (rand <= 8) {
 					((Ogre) e).setHP(((Ogre) e).getHP() - this.getDamage());
-					StatusBar.addStatus("Damaging Ogre");
+					StatusBar.addStatus("Stabbing Ogre");
 				}
 				else {
 					((Ogre) e).setHP(((Ogre) e).getHP() - this.getMaxDamage());
+				}
+			}
+			else if (e instanceof Witch) {
+				if (rand <= 8) {
+					((Witch) e).setHP(((Witch) e).getHP() - this.getDamage());
+					StatusBar.addStatus("Burning Witch");
+				}
+				else {
+					((Witch) e).setHP(((Witch) e).getHP() - this.getMaxDamage());
+				}
+			}
+			else if (e instanceof ZombieNinja) {
+				if (rand <= 8) {
+					((ZombieNinja) e).setHP(((ZombieNinja) e).getHP() - this.getDamage());
+					StatusBar.addStatus("Damaging ZombieNinja");
+				}
+				else {
+					((ZombieNinja) e).setHP(((ZombieNinja) e).getHP() - this.getMaxDamage());
 				}
 			}
 		}
