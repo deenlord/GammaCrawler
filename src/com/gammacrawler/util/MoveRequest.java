567bb7354d3bd83e3ca92640f86be6d190537954
Christian Rathke <36272735+Crathke4@users.noreply.github.com>
2018-04-30 13:13:43 -0600

    Forgot this documentation save

--- src/com/gammacrawler/util/MoveRequest.java
+++ src/com/gammacrawler/util/MoveRequest.java
@@ -7,6 +7,12 @@ public class MoveRequest {
 	public int y;
 	public Enemy e;
 
+	/**
+	 * Sets the location and enemy for the move request
+	 * @param x - x location requested
+	 * @param y - y location requested
+	 * @param e - enemy requesting to move
+	 */
 	public MoveRequest(int x, int y, Enemy e) {
 		this.x = x;
 		this.y = y;
